c72a32b2beb52046cf828adbc97045f7a3a0d3c2
d15h4nt0p <dishantmeena456@gmail.com>
2021-12-09 17:44:07 +0530

    VAMPIRE OP

--- program/callback.py
+++ program/callback.py
@@ -40,12 +40,12 @@ async def cbstart(_, query: CallbackQuery):
                         "👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
                     ),
                     InlineKeyboardButton(
-                        "📣 Official Channel", url=f"https://t.me/TheWarBotZ"
+                        "📣 Official Channel", url=f"https://t.me/Vampire_Network"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "Help", url="https://t.me/xAbhish3k"
+                        "Help", url="https://t.me/MickeyxD"
                     )
                 ],
             ]
